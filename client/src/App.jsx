699636878eae29d81ec5442cdebdef1eef51b078
pavan-teja <pavantejamadhurantakam@gmail.com>
2024-09-16 13:46:17 +0530

    added routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+
 export default function App() {
-  return <h1 className="text-red-500">App</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <h1 className="text-red-900">About</h1>;
+}
