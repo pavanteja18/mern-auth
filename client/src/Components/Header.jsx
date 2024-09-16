f6469d9429052752dcf86c68e3fbb463e76fa52b
pavan-teja <pavantejamadhurantakam@gmail.com>
2024-09-16 14:10:58 +0530

    Created Header for pages

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <>
+      <div className="bg-gray-300">
+        <div className="flex justify-between items-center p-5">
+          <Link to="/">
+            <h1 className="font-bold text-xl">Authentication</h1>
+          </Link>
+          <ul className="flex gap-4">
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/about">
+              <li>About</li>
+            </Link>
+            <Link to="/sign-in">
+              <li>Sign In</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,3 @@
 export default function About() {
-  return <h1 className="text-red-900">About</h1>;
+  return <h1>About</h1>;
 }
