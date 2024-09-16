1c082ddc6ba31e5b4c78f351be486815ef998f6a
pavan-teja <pavantejamadhurantakam@gmail.com>
2024-09-16 19:10:49 +0530

    Encrypting the password

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 const router = express.Router();
 
 router.post("/signup", async (req, res) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
     res.status(201).json({ message: "User Registration Success!!" });
